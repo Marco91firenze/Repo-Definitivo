5205e38866a4f1407563264f0efa48bfd6d1338d
Marco91firenze <marcorome91@gmail.com>
2026-04-02 03:52:10 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { LanguageProvider } from './contexts/LanguageContext';
-import { ErrorBoundary } from './components/ErrorBoundary';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ErrorBoundary>
-      <LanguageProvider>
-        <App />
-      </LanguageProvider>
-    </ErrorBoundary>
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
