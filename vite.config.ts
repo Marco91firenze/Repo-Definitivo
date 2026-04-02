3d87afee5487cbc7539e0dcd1366651a35fc6501
Marco91firenze <marcorome91@gmail.com>
2026-04-02 02:42:02 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
   }
 })
